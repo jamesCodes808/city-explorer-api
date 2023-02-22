aa08d654f9e5ce70edd6e0efd4391e4bf53cecc3
jamesCodes808 <jamesian.r.solima@gmail.com>
2023-02-21 22:15:44 -0800

    created back end api to retrieve weather data

--- server.js
+++ server.js
@@ -10,30 +10,52 @@ const PORT = process.env.PORT;
 
 app.use(cors());
 
+
+class Forecast {
+    constructor(date, description) {
+        this.date = date;
+        this.description = description;
+    }
+}
+
 app.get('/weather', (request, response) => {
-    console.log(request.url)
-    response.status(200).send('works')
-});
-
-app.get('/bad-request', (request, response) => {
     let result = null;
+    let arrOfForecast = [];
 
-    if (request.query.name) {
+    console.log(request.query)
+
+    if (request.query.city_name) {
         weather.forEach(item => {
-            if (item.name.toLowerCase() === request.query.name.toLowerCase()) {
+            if (item.city_name.toLowerCase() === request.query.city_name.toLowerCase()) {
+                console.log(item)
                 result = item;
+
+                item.data.forEach(forecast => {
+                    arrOfForecast.push(new Forecast(forecast.datetime, forecast.weather.description))
+                })
             }
         });
 
         if (!result) {
             response.status(404).send('error no data found.');
         } else {
-            response.status(200).send(result);
+            response.status(200).send(arrOfForecast);
         }
+
     } else {
         response.status(200).send(weather)
     }
 });
+
+app.get('/search', (request, response) => {
+    console.log()
+    response.send('in progress')
+})
+
+app.get('/bad-request', (request, response) => {
+    throw new Error('Testing Server Error')
+})
+
 
 app.use('*', (request, response) => {
     response.status(404).send('No data found')
